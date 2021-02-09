341f7f00587244e8de58fa9b799308816046f278
Akshay-Madrikar <akshaymadrikar@gmail.com>
2021-02-10 00:14:05 +0530

    Observables and Subject implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-recipe-app';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    console.log(feature);
-    this.loadedFeature = feature;
-  }
 }
